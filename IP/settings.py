3af4455b5850b40ed12ce1da391e9e7ea103f26e
Milena <milenabariat@mail.ru>
2021-01-18 22:54:41 +0300

    final

--- IP/settings.py
+++ IP/settings.py
@@ -143,8 +143,11 @@ GRAPH_MODELS = {
   'group_models': True,
 }
 
-
+import os.path
+import sys
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
